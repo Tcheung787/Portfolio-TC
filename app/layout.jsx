6a58058318cb7305218006af37cb85067be6ce7f
Tcheung787 <tiffanycheung02@gmail.com>
2024-09-12 22:46:39 -0400

    Added dark/light mode

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,6 +1,14 @@
 import localFont from "next/font/local";
 import "./globals.css";
 
+
+// components
+import Header from '@/components/Header';
+import Footer from '@/components/Footer';
+
+// theme provider
+import { ThemeProvider } from "@/components/ThemeProvider";
+
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
   variable: "--font-geist-sans",
@@ -19,12 +27,15 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+        <ThemeProvider attribute='class' defaultTheme= 'light'>
+          <Header />
+          {children}
+          <Footer/>
+        </ThemeProvider>
+        </body>
     </html>
   );
 }
