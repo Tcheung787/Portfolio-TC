c3d4c80296b442a987c72d0aac8b9cf6e7b1fe4a
Tcheung787 <tiffanycheung02@gmail.com>
2024-09-24 18:51:01 -0400

    ugh

--- components/About.jsx
+++ components/About.jsx
@@ -170,10 +170,10 @@ const About = () => {
 
   return (
 
-    <section className='xl:h-[860px] pb-12 xl:py-24'>
+    <section className='xl:h-[860px] pb-12 xl:py-24 mt-22'>
       <div className='container mx-auto'>
         <h2 className='section-title mb-8 xl:mb-16 text-center mx-auto'>
-      About Me
+          About me
     </h2>
     <div className='flex flex-col xl:flex-row'>
       {/* image */}
