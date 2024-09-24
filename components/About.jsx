9dcf68852454df47140d77bee17e613dc702e07b
Tcheung787 <tiffanycheung02@gmail.com>
2024-09-24 19:16:32 -0400

    changes

--- components/About.jsx
+++ components/About.jsx
@@ -169,7 +169,7 @@ const About = () => {
   };
 
   return (
-    <section className='relative top-12 sm:top-24 xl:h-[860px] pb-12 xl:py-24'>
+    <section className='relative top-25 sm:top-25 xl:h-[860px] pb-12 xl:py-24'>
       <div className='container mx-auto'>
         <h2 className='section-title mb-8 xl:mb-16 text-center mx-auto'>
           About me
